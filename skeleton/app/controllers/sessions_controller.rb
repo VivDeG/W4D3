c4284c8790f7cc05a66c395f2270c44a9b23bd21
VivDeG <vivienedeg@gmail.com>
2019-01-30 16:46:13 -0800

    Add authentication to the 99cats

---
+++ skeleton/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+    render :new 
+  end
+  
+  def create
+    if params[:user].nil?
+      render :new
+    else
+      user = User.find_by_credentials(
+        params[:user][:username],
+        params[:user][:password]
+      )
+    
+
+    if user.nil?
+      render json: 'Credentials were wrong'
+    else
+      login!(user)
+      redirect_to cats_url
+    end
+  end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+end
---
+++ skeleton/app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+
+  has_many :cats,
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: :Cat
+  
+
+  after_initialize do |user|
+   user.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+   # user.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+end
